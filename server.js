19889717abbbd474ce5f72eef9ef0ca7faddf579
Nick <57273851+nick555555@users.noreply.github.com>
2020-02-24 21:13:21 -0800

    server set up

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log(`App listening on Port ${PORT}`);
+});
